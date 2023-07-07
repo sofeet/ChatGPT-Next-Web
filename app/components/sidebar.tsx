d0dce3e4429d4c7355ee24ca998a32ac56255440
sofeet <7344662@qq.com>
2023-07-07 16:45:46 +0800

    最近更新2023-0707

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,10 +117,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          蝶雨AI人工智能
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          致力为中文chatgpt提供动力.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
